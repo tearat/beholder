f576cd570f3e7927b5f769be64f188662a71439b
Teraaret <raiiliiat@yandex.ru>
2020-10-02 10:37:15 +0500

    Add --time option, clear dublicate code for --get option

--- beholder.cr
+++ beholder.cr
@@ -5,12 +5,14 @@ require "colorize"
 options = {} of Symbol => String
 filename = File.join(File.dirname(__FILE__), "/list.txt")
 
+
 begin
     OptionParser.parse do |parser|
         parser.banner = "Usage: sauron.rb [options]"
         parser.on("-g URL", "--get=URL", "Scan selected URL") { |url| options[:get] = url }
         parser.on("-a", "--all", "Scan all sites in the list") { options[:all] = "true" }
         parser.on("-f FILE", "--file=FILE", "Specify file to use") { |file| options[:file] = file }
+        parser.on("-t", "--time", "Shows elapsed time") { options[:time] = "true" }
         parser.on("-h", "--help", "Show this help") { puts parser }
     end
 rescue error : OptionParser::InvalidOption
@@ -25,33 +27,28 @@ if options.empty?
 end
 
 
-if options.has_key?(:file)
-    filename = options[:file]
+all_arg  = options.has_key?(:all)  ? true : nil
+file_arg = options.has_key?(:file) ? options[:file] : nil
+get_arg  = options.has_key?(:get)  ? options[:get]  : nil
+time_arg = options.has_key?(:time) ? options[:time]  : nil
+
+
+# Overwrite list
+if file_arg
+    filename = file_arg
 end
 
 
-if options.has_key?(:get)
-    site = options[:get]
-    begin
-        response = HTTP::Client.get site
-        code = response.status_code
-        if /[2,3].{2}/ =~ code.to_s
-            puts "#{site} => #{code.to_s.colorize.green}"
-        else
-            puts "#{site} => #{code.to_s.colorize.red}"
-        end
-    rescue error : Socket::Addrinfo::Error
-        puts "#{site} => #{"Host not found".colorize.red}"
-    end
-end
-
-
-if options.has_key?(:all)
+if all_arg || get_arg
     sites = [] of String
-    begin
-        sites = File.read_lines(filename)
-    rescue error : File::NotFoundError
-        puts "File #{filename} not found".colorize.red
+    if get_arg
+        sites.push get_arg
+    else
+        begin
+            sites = File.read_lines(filename)
+        rescue error : File::NotFoundError
+            puts "File #{filename} not found".colorize.red
+        end
     end
 
     max_len = 0
@@ -64,12 +61,22 @@ if options.has_key?(:all)
 
     sites.each do |site|
         begin
+            start_time = Time.local.millisecond
             response = HTTP::Client.get site
+            elapsed_time = Time.local.millisecond - start_time
             code = response.status_code
             if /[2,3].{2}/ =~ code.to_s
-                puts "#{site.ljust(max_len)}=> #{code.to_s.colorize.green}"
+                report = "#{site.ljust(max_len)}=> #{code.to_s.colorize.green}"
+                if time_arg
+                    report += " [ #{elapsed_time} ms ]"
+                end
+                puts report
             else
-                puts "#{site.ljust(max_len)}=> #{code.to_s.colorize.red}"
+                report = "#{site.ljust(max_len)}=> #{code.to_s.colorize.red}"
+                if time_arg
+                    report += " [ #{elapsed_time} ms ]"
+                end
+                puts report
             end
         rescue error : Socket::Addrinfo::Error
             puts "#{site.ljust(max_len)}=> #{"Host not found".colorize.red}"
